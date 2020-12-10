9f47d1495d3d9d7893b4b948d49c8e717716a588
agnc.clint <agnc.clint@gmail.com>
2020-12-11 03:43:09 +0800

    Documentation process!

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,8 @@
-import React, {useState} from 'react';
-import { EmptyState, Layout, Page } from '@shopify/polaris';
-import { ResourcePicker } from '@shopify/app-bridge-react';
-function Index(){
-    // const [modal, setModal]
-    return(
-        <div>
-            <EmptyState
-            heading="Manage your inventory transfers"
-            action={{content: 'Add transfer'}}
-            secondaryAction={{content: 'Learn more', url: 'https://help.shopify.com'}}
-            image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
-            >
-            <p>Track and receive your incoming inventory from suppliers.</p>
-            </EmptyState>
-        </div>
-    )
-}
-
-export default Index;
+  const Index = () => (
+    <div>
+      <p>Sample app using React and Next.js</p>
+    </div>
+  );
+  
+  export default Index;
+  
